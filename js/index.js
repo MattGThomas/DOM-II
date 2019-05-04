b50ab9f0b7fbb2228b5ea11bbac0e36f988d77c6
MattGThomas <matthewgthomas0616@gmail.com>
2019-05-04 14:56:27 -0500

    added two more event listeners

--- js/index.js
+++ js/index.js
@@ -7,4 +7,25 @@ navLink.addEventListener('mouseover', function(e){
 const button = document.querySelector('.btn');
 button.addEventListener('dblclick', function(e){
     button.classList.toggle('large');
+    button.textContent = 'Click here for fun!'
 });
+
+const button2 = document.querySelector('#btn2');
+button2.addEventListener('click', function(e){
+    button2.classList.toggle('large');
+    button2.textContent = 'Pack your bags!';
+});
+
+const button3 = document.querySelector('#btn3');
+button3.addEventListener('mouseover', function(e){
+    button3.textContent = 'Island Time'
+});
+
+// function logSelection(event){
+//     const log = document.querySelector('#log');
+//     const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+//     log.textContent = `You selected: ${selection}`;
+// }
+// const introduct = document.querySelector('.intro');
+// introduct.addEventListener('select', logSelection)
+
