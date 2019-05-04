237f48f3b87ebf2868b6d64eef45b276763519af
MattGThomas <matthewgthomas0616@gmail.com>
2019-05-04 13:57:53 -0500

    added mouseover and dblclick eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const navLink = document.querySelector('.nav-link');
+navLink.addEventListener('mouseover', function(e){
+    navLink.style.color = 'red';
+});
+
+const button = document.querySelector('.btn');
+button.addEventListener('dblclick', function(e){
+    button.classList.toggle('large');
+});
