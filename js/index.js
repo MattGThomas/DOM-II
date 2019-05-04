eb992d183f9b6123f41c4fdc230de40953066e6f
MattGThomas <matthewgthomas0616@gmail.com>
2019-05-04 15:22:35 -0500

    added more event listeners

--- js/index.js
+++ js/index.js
@@ -1,31 +1,45 @@
 // Your code goes here
+
+/*----Navigation Link Mouseover----*/
 const navLink = document.querySelector('.nav-link');
 navLink.addEventListener('mouseover', function(e){
     navLink.style.color = 'red';
 });
 
+/*----Button 1 Size Changer----*/
 const button = document.querySelector('.btn');
 button.addEventListener('dblclick', function(e){
     button.classList.toggle('large');
     button.textContent = 'Click here for fun!'
 });
 
+/*----Button 2 text and size changer----*/
 const button2 = document.querySelector('#btn2');
 button2.addEventListener('click', function(e){
     button2.classList.toggle('large');
     button2.textContent = 'Pack your bags!';
 });
 
+/*----Button 3 Text Changer----*/
 const button3 = document.querySelector('#btn3');
 button3.addEventListener('mouseover', function(e){
     button3.textContent = 'Island Time'
 });
 
-// function logSelection(event){
-//     const log = document.querySelector('#log');
-//     const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-//     log.textContent = `You selected: ${selection}`;
-// }
-// const introduct = document.querySelector('.intro');
-// introduct.addEventListener('select', logSelection)
+/*----Change Intro Paragraph Color----*/
+const intro = document.querySelector('#introduction-text');
+intro.addEventListener('click', function(e){
+    intro.style.color = 'blue'
+});
+intro.addEventListener('dblclick', function(e){
+    intro.style.color = 'red'
+});
 
+/*----Window Sizer----*/
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+window.onresize = reportWindowSize;
